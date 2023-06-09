1d2e25dec706a345e9e335b8fad9aece46ef6e1c
Matthew Zhang <matthewzhang0226@yahoo.com>
2023-06-09 15:23:32 -0500

    first commit, setup

---
+++ src/main/java/com.amigoscode/Main.java
@@ -0,0 +1,10 @@
+package main.java.com.amigoscode;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
