93122ec714085e4b545753a826f3e7a7f23b23f3
Matthew Zhang <matthewzhang0226@yahoo.com>
2023-06-12 13:51:54 -0500

    start db configuration

--- src/main/java/com.amigoscode/Main.java
+++ src/main/java/com.amigoscode/Main.java
@@ -1,10 +1,42 @@
-package main.java.com.amigoscode;
+package com.amigoscode;
 
+import java.util.List;
+
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello");
+        SpringApplication.run(Main.class, args);
+    }
+
+    // api url: http//localhost:8080/greet
+    @GetMapping("/greet")
+    public GreetResponse greet() {
+        //returns json object: {"greet":"Hello"}
+        GreetResponse response = new GreetResponse(
+            "Hello",
+            List.of("Java", "Golang", "Javascript"),
+            new Person("Alex")
+        );
+
+        return response;
+    }
+
+    record Person(String name)
+    {
+
+    }
+
+    record GreetResponse(
+        String greet, 
+        List<String> favProgrammingLanguages,
+        Person person
+    ) {
+
     }
 }
